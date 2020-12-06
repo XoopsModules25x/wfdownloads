c188d2c6463bb685565d1b3b0a8b1cf65c90d7f3
mambax7 <mambax7@gmail.com>
2020-12-06 13:41:47 -0500

    fix for missing XoopsUserUtility

--- header.php
+++ header.php
@@ -31,6 +31,7 @@ require_once dirname(__DIR__, 2) . '/mainfile.php';
 require_once __DIR__ . '/include/common.php';
 
 //require_once XOOPS_ROOT_PATH . '/header.php';
+xoops_load('XoopsUserUtility');
 
 $moduleDirName = basename(__DIR__);
 $moduleDirNameUpper = mb_strtoupper($moduleDirName);
